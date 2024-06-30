61a802be78981db19641c1c994a6e777a1e4ef11
Roman Zberg <roman@zberg.ch>
2024-06-30 09:38:35 +0200

    changed test script, so that it can test multiple models

--- src/test_dino_v2_based_model.py
+++ src/test_dino_v2_based_model.py
@@ -92,52 +92,65 @@ def vaildate_on_polyvore_dataset(model, polyvore_df: pd.DataFrame, dataset_folde
 
 
 def main(
-        pth_file,
-        testing_dataset_path,
+        pth_files,
+        testing_dataset_paths,
         zalando_dataset_path,
         polyvore_datasets_folder_root_path,
         zalando_datasets_folder_root_path
 ):
-    wandb.init(
-        # set the wandb project where this run will be logged
-        project="ReWear - Outfit Recommender (DSPRO2) Dino v2 based testing",
-        config={
-            'pth_file': pth_file
+    for index, pth_file in enumerate(pth_files):
+        wandb.init(
+            # set the wandb project where this run will be logged
+            project="ReWear - Outfit Recommender (DSPRO2) Dino v2 based testing",
+            config={
+                'pth_file': pth_file,
+                'testing_dataset_path': testing_dataset_paths[index],
+                'zalando_dataset_path': zalando_dataset_path
+            }
+        )
+
+        model = OutfitClassifier.create_from_pth_file(pth_file, device).eval()
+
+        polyvore_test_dataset_df = pd.read_parquet(testing_dataset_paths[index])
+        zalando_test_dataset_df = pd.read_parquet(zalando_dataset_path)
+
+        result_dict_polyvore = vaildate_on_polyvore_dataset(
+            model, polyvore_test_dataset_df,
+            polyvore_datasets_folder_root_path
+        )
+
+        result_dict_zalando = vaildate_on_zalando_dataset(
+            model, zalando_test_dataset_df, zalando_datasets_folder_root_path
+        )
+
+        log_dict = {
+            **result_dict_polyvore,
+            **result_dict_zalando
         }
-    )
 
-    model = OutfitClassifier.create_from_pth_file(pth_file, device).eval()
+        wandb.log(
+            log_dict
+        )
 
-    polyvore_test_dataset_df = pd.read_parquet(testing_dataset_path)
-    zalando_test_dataset_df = pd.read_parquet(zalando_dataset_path)
+        print(log_dict)
 
-    result_dict_polyvore = vaildate_on_polyvore_dataset(
-        model, polyvore_test_dataset_df,
-        polyvore_datasets_folder_root_path
-    )
-
-    result_dict_zalando = vaildate_on_zalando_dataset(
-        model, zalando_test_dataset_df, zalando_datasets_folder_root_path
-    )
-
-    log_dict = {
-        **result_dict_polyvore,
-        **result_dict_zalando
-    }
-
-    wandb.log(
-        log_dict
-    )
-
-    print(log_dict)
-
-    wandb.finish()
+        wandb.finish()
 
 
 if __name__ == "__main__":
     main(
-        pth_file='../trained_models/volanic-moon-9-median-threshold.pth',
-        testing_dataset_path='../datasets/imageBasedModel/polyvore/median_threshold_eb26e630100b98397deda54fa4a0bb95929479bc30e83cbfa72424b7c1e6e178/polyvore_test_6b046cdf467634343bb4fdd8fbdbe02a3746645c7c9108242228da408c97f435.parquet',
+        pth_files=[
+            '../trained_models/volanic-moon-9-median-threshold.pth',
+            '../trained_models/honest-grass-25-median-threshold.pth',
+            '../trained_models/cosmic-sunset-49-q1-threshold.pth',
+            '../trained_models/lively-planet-39-q1-threshold.pth'
+        ],
+        testing_dataset_paths=[
+            '../datasets/imageBasedModel/polyvore/median_threshold_eb26e630100b98397deda54fa4a0bb95929479bc30e83cbfa72424b7c1e6e178/polyvore_test_6b046cdf467634343bb4fdd8fbdbe02a3746645c7c9108242228da408c97f435.parquet',
+            '../datasets/imageBasedModel/polyvore/median_threshold_eb26e630100b98397deda54fa4a0bb95929479bc30e83cbfa72424b7c1e6e178/polyvore_test_6b046cdf467634343bb4fdd8fbdbe02a3746645c7c9108242228da408c97f435.parquet',
+            '../datasets/imageBasedModel/polyvore/q1_threshold_oversampled_93bb63f88b5735b75cbac6b4549ca7de6cb6759b70cdd99cb2efaa2cad1ea359/polyvore_test_eccba23b76ec907bfc446b3ff374930b55c2d9609d33fac87bccf5b2fb95db71.parquet',
+            '../datasets/imageBasedModel/polyvore/q1_threshold_oversampled_93bb63f88b5735b75cbac6b4549ca7de6cb6759b70cdd99cb2efaa2cad1ea359/polyvore_test_eccba23b76ec907bfc446b3ff374930b55c2d9609d33fac87bccf5b2fb95db71.parquet'
+        ],
         zalando_dataset_path='../datasets/imageBasedModel/zalando/zalando_ea4bf73acd0119c31a8a708bce2a2b302e72c1ecebc7c2265ee80cff3a15ed0a.parquet',
         polyvore_datasets_folder_root_path='../datasets',
         zalando_datasets_folder_root_path='../datasets/imageBasedModel/zalando'
